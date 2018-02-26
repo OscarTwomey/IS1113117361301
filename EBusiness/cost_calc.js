ba8fdf68f15bcf10f0000232d04bfc05c66f3c1f
Oscar Twomey <117361301@umail.ucc.ie>
2018-02-26 20:10:14 +0000

    Pushing changes

---
+++ EBusiness/cost_calc.js
@@ -0,0 +1,70 @@
+/*global $*/
+
+
+function calcSub(){
+    
+    var argSubTotal;
+    
+    if(document.getElementById('salesforce').checked) {
+        argSubTotal = 100;
+    }
+    else {
+        argSubTotal = 100;
+    }
+    display(argSubTotal);
+    
+    
+}
+
+function display(parm1){
+    
+    document.getElementById("subtotal").value = parm1;
+    document.getElementById("total").value = parm1;
+    
+    enablebtnProceed();
+}
+
+
+function enablebtnProceed(){
+    $('#btnProceed').prop('disabled', false);
+}
+
+function disablebtnProceed(){
+    $('#btnProceed').prop('disabled', true);
+}
+
+/* global $*/
+function calcSub(){
+    var argSubTotal;
+    
+    if(document.getElementById('salesforce').checked) {
+        argSubTotal = 100;
+    }
+    else if (document.getElementById('gmail').checked) {
+        argSubTotal = 400;
+    }
+    else if (document.getElementById('cloud 9').checked) {
+        argSubTotal = 200;
+    }
+    else  { 
+        argSubTotal = 300;
+    }
+    
+    display(argSubTotal);
+    }
+    
+function display(parm1){
+    
+   document.getElementById("subtotal").value= parm1 ;
+   document.getElementById("total").value= parm1 ;
+   
+   enablebtnProceed();
+}
+
+function enablebtnProceed(){
+    $('#btnProceed').prop('disabled', false);
+}
+
+function disablebtnProceed(){
+     $('#btnProceed').prop('disabled', true);
+}
